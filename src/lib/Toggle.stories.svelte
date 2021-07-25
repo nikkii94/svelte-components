b9b6a4d588ff21ffa7a6a1abba8dd21707db555a
Niki <k.nikolett17@gmail.com>
2021-07-25 17:00:44 +0200

    Initializing storybook

---
+++ src/lib/Toggle.stories.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+    import Toggle from './Toggle.svelte';
+</script>
+
+<Meta title="Toggle" component={Toggle} />
+
+<Template let:args>
+    <Toggle {...args} on:click={args.onClick} />
+</Template>
+
+<Story name="Default" />
+
+<Story name="With props" args={{
+    label: 'Default toggle label',
+    style: '--background: blue; --toggledButtonColor: red'
+}}/>
