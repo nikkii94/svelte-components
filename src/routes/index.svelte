abdf0763419e9cdd59b87b14977462166c0ebec4
nikkii94 <k.nikolett17@gmail.com>
2022-08-28 16:59:23 +0200

    MultiSelect component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,6 +9,7 @@
     import Modal from "$lib/Modal.svelte";
     import Toast from "$lib/Toast/Toast.svelte";
     import { toast } from "$lib/Toast/toast";
+    import MultiSelect from '$lib/MultiSelect/MultiSelect.svelte';
 
     let isToggled = false;
 
@@ -27,52 +28,55 @@
     let isModalOpen = false;
 </script>
 
-<h1>Toggle Component</h1>
+<MultiSelect></MultiSelect>
 
-<Toggle bind:isToggled label="Toggle label"/>
-<Toggle bind:isToggled label="Toggle label 2" style="--background: red; --toggledButtonColor: green"/>
+    <h1>Toggle Component</h1>
+    
+    <Toggle bind:isToggled label="Toggle label"/>
+    <Toggle bind:isToggled label="Toggle label 2" style="--background: red; --toggledButtonColor: green"/>
+    
+    {#if isToggled}
+        <h2>is toggled</h2>
+    {/if}
+    
+    <!--<h1>Accordion Component</h1>-->
+    <!--<Accordion buttonText="+">-->
+    <!--    <p>-->
+    <!--        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.-->
+    <!--    </p>-->
+    <!--</Accordion>-->
+    <!--<Accordion isOpen={false} buttonText="+">-->
+    <!--    <p>-->
+    <!--        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.-->
+    <!--    </p>-->
+    <!--</Accordion>-->
+    
+    <h1>BetterAccordion Component</h1>
+    <BetterAccordion isOpen={false} buttonText="Toggle this button">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    </BetterAccordion>
+    
+    <h1>Search & Filter: {search}</h1>
+    <SearchFilter style="--listItemBackground: aqua" items={items} bind:search />
+    
+    <h1>Portal</h1>
+    <Portal>
+        <h1>Markdown</h1>
+        <Markdown bind:text={markdownText} />
+    </Portal>
+    
+    <h1>Modal</h1>
+    <button on:click={() => isModalOpen = true}>Open modal</button>
+    <Modal bind:isModalOpen background={true}>
+        <h1>Input Field</h1>
+        <Field bind:value={search} label="Search" instructions="Type to search..." placeholder="Joe" />
+        <Field value={0} label="Number" type="number" instructions="Type to search..." placeholder="Joe" />
+    </Modal>
+    
+    <h1>Toast</h1>
+    <button on:click={() => toast.send('New message ' + Math.random())}>Add toast</button>
+    
+    <Toast duration={3000} />
 
-{#if isToggled}
-    <h2>is toggled</h2>
-{/if}
-
-<!--<h1>Accordion Component</h1>-->
-<!--<Accordion buttonText="+">-->
-<!--    <p>-->
-<!--        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.-->
-<!--    </p>-->
-<!--</Accordion>-->
-<!--<Accordion isOpen={false} buttonText="+">-->
-<!--    <p>-->
-<!--        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.-->
-<!--    </p>-->
-<!--</Accordion>-->
-
-<h1>BetterAccordion Component</h1>
-<BetterAccordion isOpen={false} buttonText="Toggle this button">
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
-</BetterAccordion>
-
-<h1>Search & Filter: {search}</h1>
-<SearchFilter style="--listItemBackground: aqua" items={items} bind:search />
-
-<h1>Portal</h1>
-<Portal>
-    <h1>Markdown</h1>
-    <Markdown bind:text={markdownText} />
-</Portal>
-
-<h1>Modal</h1>
-<button on:click={() => isModalOpen = true}>Open modal</button>
-<Modal bind:isModalOpen background={true}>
-    <h1>Input Field</h1>
-    <Field bind:value={search} label="Search" instructions="Type to search..." placeholder="Joe" />
-    <Field value={0} label="Number" type="number" instructions="Type to search..." placeholder="Joe" />
-</Modal>
-
-<h1>Toast</h1>
-<button on:click={() => toast.send('New message ' + Math.random())}>Add toast</button>
-
-<Toast duration={3000} />
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,22 @@
+import preprocess from 'svelte-preprocess';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+		target: '#svelte',
+		vite: {
+			optimizeDeps: {
+			  exclude: ['sswr']
+			}
+		  }
+	},
+	preprocess: preprocess({
+		defaults: {
+			style: 'postcss',
+		},
+		postcss: true,
+	})
 };
 
 export default config;
